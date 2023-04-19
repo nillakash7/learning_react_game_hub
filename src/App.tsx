5b1b387853476acb19f1dae6e3d64315d375da14
Md. Fazlur Rahman <nill_akash_7@hotmail.com>
2023-04-19 16:51:18 +0400

    install chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
 const App = () => {
-  return <div>Hello gaming portal</div>;
+  return (
+    <div>
+      Hello gaming portal
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 };
 
 export default App;
