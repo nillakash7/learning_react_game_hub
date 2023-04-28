edf8834f530320155775a962154badaa5a614a69
Md. Fazlur Rahman <nill_akash_7@hotmail.com>
2023-04-28 10:09:03 +0400

    Responsive UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
   return (
-    <div>
-      Hello gaming portal
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "footer"`,
+        lg: `"nav nav" "aside main" "footer footer"`,
+      }}
+    >
+      <GridItem area="nav" bg="blue">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="green">
+          Menu
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="red">
+        Body
+      </GridItem>
+      <GridItem area="footer" bg="purple">
+        Footer
+      </GridItem>
+    </Grid>
   );
 };
 
