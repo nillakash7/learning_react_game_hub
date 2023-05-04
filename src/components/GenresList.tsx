3bcc00ca20adc893df5b31fa7493b4bfdcf0d79f
Md. Fazlur Rahman <nill_akash_7@hotmail.com>
2023-05-04 15:28:23 +0400

    clean up genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,4 +1,11 @@
-import { HStack, Image, List, ListItem, Button } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
 import useGenres, { Genres } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import { GenresItemShadow } from "./GenresItemShadow";
@@ -14,28 +21,36 @@ export const GenresList = ({ onSelectGenres, selectedGenres }: Props) => {
   if (error) return null;
   if (isLoading) return <GenresItemShadow></GenresItemShadow>; //return <Spinner></Spinner>;
   return (
-    <List>
-      {data.map((item) => (
-        <ListItem key={item.id} marginY={3}>
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius="5px"
-              src={getCroppedImageUrl(item.image_background)}
-            ></Image>
-            <Button
-              variant="link"
-              fontSize="lg"
-              fontWeight={selectedGenres == item.id ? "bold" : "normal"}
-              onClick={() => {
-                onSelectGenres(item);
-              }}
-            >
-              {item.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading marginBottom={3} fontSize={"2xl"}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((item) => (
+          <ListItem key={item.id} marginY={3}>
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius="5px"
+                objectFit="cover"
+                src={getCroppedImageUrl(item.image_background)}
+              ></Image>
+              <Button
+                variant="link"
+                fontSize="lg"
+                fontWeight={selectedGenres == item.id ? "bold" : "normal"}
+                whiteSpace="normal"
+                textAlign="left"
+                onClick={() => {
+                  onSelectGenres(item);
+                }}
+              >
+                {item.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
